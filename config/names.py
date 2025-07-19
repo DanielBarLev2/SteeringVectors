3cbacfe021ba4acc9af68b2d25d35aafe88a23e5
DanielBarLev <danielbarlev00@gmail.com>
2025-07-19 11:51:54 +0300

    add hookig names

--- config/names.py
+++ config/names.py
@@ -4,10 +4,14 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # MODELS
 LLAMA_2_7B = "meta-llama/Llama-2-7b-chat-hf"
-GAMMA_2_2B = "gemma/gemma-2-2b"
+GEMMA_2_2B = "google/gemma-2b"
 
 # DATA
 ALPACA_PATH = "tatsu-lab/alpaca"
 MAL_PATH  = "walledai/MaliciousInstruct"
 ADV_PATH = "walledai/AdvBench"
 TDC_PATH = "walledai/TDC23-RedTeaming"
+
+LAYER_IDX = 14
+DATASET_SIZE = 128
+BATCH_SIZE = 8 # for hooking
